069c7a2cb6a495b28bf761a7a99124920b8429d0
Eyal Azran <azran4u@gmail.com>
2022-05-22 18:23:34 +0300

    speech to text module

---
+++ src/speech-to-text/speech.to.text.gcp.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@nestjs/common';
+const speech = require('@google-cloud/speech');
+
+@Injectable()
+export class SpeechToTextGcpService {
+  private client;
+
+  constructor() {
+    this.client = new speech.SpeechClient();
+  }
+
+  async transcribe(urlToCloudStorage: string): Promise<string[]> {
+    const audio = {
+      uri: urlToCloudStorage,
+    };
+    const config = {
+      languageCode: 'iw-IL',
+    };
+    const request = {
+      audio: audio,
+      config: config,
+    };
+
+    const [response] = await this.client.recognize(request);
+    return response.results;
+  }
+}
