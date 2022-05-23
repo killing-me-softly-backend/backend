c3c6508661291abe13e06da61a6d92ada16acc18
Tamir Arie <tamirarie13@gmail.com>
2022-05-23 15:17:29 +0300

    added diarydto fields

--- src/entities/events/create.event.dto.ts
+++ src/entities/events/create.event.dto.ts
@@ -1,7 +1,15 @@
+import { MediaEvent } from "./media.event";
+
 export class CreateDiaryEventDto {
   readonly display_name: string;
-  readonly timestamp: string;
+  readonly timestamp: Date;
   readonly results: string[];
+  readonly image: MediaEvent;
+  readonly video: MediaEvent;
+  readonly audio: MediaEvent;
+  readonly audio_trancsribe: string;
+  readonly offensive_words: string[];
+  readonly free_text: string;
 
 }
 
