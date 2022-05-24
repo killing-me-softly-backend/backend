ed52c7588cd67ef0dd6df75fc074fd3463bba16f
Tsur Ost <me@melech.com>
2022-05-24 14:25:38 +0300

    changed port config

--- src/config/config.factory.ts
+++ src/config/config.factory.ts
@@ -5,7 +5,7 @@ export const configFactory: ConfigFactory<{ config: Configuration }> = () => {
   return {
     config: {
       server: {
-        port: envToNumberOrDefault('SERVER_PORT', 3500),
+        port: envToNumberOrDefault('PORT', 3500),
       },
       logger: {
         level: envToStringOrDefault('LOGGER_LEVEL', 'info'),
